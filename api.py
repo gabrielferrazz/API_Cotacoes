841dfc3c745426c678c8a1c784b94164199063ba
Gabriel Ferraz <135299312+gabrielferrazz@users.noreply.github.com>
2023-07-25 09:52:21 -0300

    Update api.py

--- api.py
+++ api.py
@@ -2,14 +2,14 @@ import requests # biblioteca Requests
 import json # biblioteca Json
 
 cotacoes = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL") # Onde fica as informações das cotações
-cotacoes = cotacoes.json() # convertendo as cotações para um Json
+cotacoes = cotacoes.json() # Convertendo as cotações para um Json
 
-cotacao_dolar = cotacoes['USDBRL']["bid"] # Solicitnado a cotação em Dolar
+cotacao_dolar = cotacoes['USDBRL']["bid"] # Solicitando a cotação em Dolar
 print(f'Cotaçao do Dolar atual: R$ {cotacao_dolar}') # Mostrando a cotação em Dolar
 
-cotacao_euro = cotacoes['EURBRL']["bid"] # Solicitnado a cotação em Euro
+cotacao_euro = cotacoes['EURBRL']["bid"] # Solicitando a cotação em Euro
 print(f'Cotaçao do Euro atual: R$ {cotacao_euro}') # Mostrando a cotação em Euro
 
-cotacao_bitcoin = cotacoes['BTCBRL']["bid"] # Solicitnado a cotação em Bitcoin
+cotacao_bitcoin = cotacoes['BTCBRL']["bid"] # Solicitando a cotação em Bitcoin
 print(f'Cotaçao do Bitocin atual: R$ {cotacao_bitcoin}') # Mostrando a cotação em Bitcoin
 
